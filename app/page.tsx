29d3f810593340846043206cfa458cbc77be5fbc
Fred <frederik.koenig@posteo.de>
2024-08-27 10:29:10 +0200

    created frontend for filter drawer, also put filter icon on the map

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { FilterDrawer } from "@/components/FilterDrawer";
 import { DrawerHomepage } from "@/components/DrawerHomepage";
 import Navbar from "../components/Navbar";
 import { Map } from "@/components/Map";
@@ -5,6 +6,9 @@ import { Map } from "@/components/Map";
 export default function Home() {
   return (
     <div className="h-screen w-full">
+      <Map>
+        <FilterDrawer />
+      </Map>
       <Map />
       <DrawerHomepage />
       <Navbar />
